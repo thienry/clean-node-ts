a41c441dfb1bc116dfcce4cd5fe5e060682d7cea
Thiago Moura <33002497+thienry@users.noreply.github.com>
2021-09-12 12:19:25 -0300

    Feature/signup api presentation layer (#1)
    * chore: add test clear cache command
    
    * feat: ensure SignUpController returns 400 if no name is provided
    
    * test: ensure SignUpController returns 400 if no name is provided
    
    * feat: ensure SignUpController returns an error if no name is provided
    
    * test: ensure SignUpController returns an error if no name is provided
    
    * feat: ensure SignUpController returns an error if no email is provided
    
    * test: ensure SignUpController returns an error if no email is provided
    
    * refactor: ensure SignUpController.handle implement correct interfaces
    
    * refactor: add custom error for missing param
    
    * refactor: move duplicated code to a helper method
    
    * test: add coverage files
    
    * refactor: simplify code
    
    * feat: ensure SignUpController returns 400 if no password/passwordConfirmation is provided
    
    * test: ensure SignUpController returns 400 if no password/passwordConfirmation is provided
    
    * refactor: make SignUpController conforms to a protocol
    
    * refactor: move sut creation to a factory helper method
    
    * feat: ensure SignUpController returns 400 if an invalid email is provided
    
    * test: ensure SignUpController returns 400 if an invalid email is provided
    
    * test: add coverage files
    
    * test: ensure SignUpController calls EmailValidator with correct email
    
    * feat: ensure SignUpController returns 500 if EmailValidator throws an error
    
    * test: ensure SignUpController returns 500 if EmailValidator throws an error
    
    * test: add coverage files
    
    * refactor: move server error to a helper method
    
    * refactor: move all error imports to a single file
    
    * refactor: move all protocol imports to a single file
    
    * refactor: move email validation creation to a factory helper method
    
    * test: add coverage files
    
    * refactor: change mocking strategy
    
    * feat: ensure SignUpController returns 400 if password confirmation fails
    
    * test: ensure SignUpController returns 400 if password confirmation fails
    
    * refactor: remove code duplication
    
    * feat: ensure SignUpController calls AddAcount with correct values
    
    * test: ensure SignUpController calls AddAcount with correct values
    
    * refactor: move all signup protocols to a single file
    
    * test: add coverage files
    
    * test: ensure SignUpController returns 500 if AddAccount returns an error
    
    * feat: ensure SignUpController returns 201 if valid data is provided
    
    * test: ensure SignUpController returns 201 if valid data is provided
    
    * refactor: move success created http response to a helper method
    
    * refactor: transformer handle to async
    
    * refactor: change imports inside HttpProtocol
    
    * test: add coverage files

---
+++ src/presentation/controllers/signup/signup.controller.ts
@@ -0,0 +1,34 @@
+import { badRequest, created, serverError } from '../../helpers/http.helper'
+import { InvalidParamError, MissingParamError } from '../../errors'
+import { AddAccount, Controller, EmailValidator, HttpResponse, httpRequest } from './signup.protocols'
+
+export class SignUpController implements Controller {
+  constructor(
+    private readonly addAccount: AddAccount,
+    private readonly emailValidator: EmailValidator
+  ) {}
+
+  async handle(httpRequest: httpRequest): Promise<HttpResponse> {
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
+      }
+
+      const { name, email, password, passwordConfirmation } = httpRequest.body
+
+      if (password !== passwordConfirmation) return badRequest(new InvalidParamError('passwordConfirmation'))
+
+      const isValid = this.emailValidator.isValid(email)
+
+      if (!isValid) return badRequest(new InvalidParamError('email'))
+
+      const account = await this.addAccount.add({ name, email, password })
+
+      return created(account)
+    } catch (error) {
+      return serverError()
+    }
+  }
+}
---
+++ src/presentation/helpers/http.helper.ts
@@ -0,0 +1,6 @@
+import { ServerError } from '../errors'
+import { HttpResponse } from '../protocols'
+
+export const created = (data: any): HttpResponse => ({ statusCode: 201, body: data })
+export const badRequest = (error: Error): HttpResponse => ({ statusCode: 400, body: error })
+export const serverError = (): HttpResponse => ({ statusCode: 500, body: new ServerError() })
